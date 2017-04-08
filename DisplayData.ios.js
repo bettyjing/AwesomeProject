676d5a483697900ac16cab1c72e8ba6d253781af
bettyjing <you@example.com>
2017-04-08 14:06:58 -0700

    network request failed still
    : - )

--- DisplayData.ios.js
+++ DisplayData.ios.js
@@ -13,87 +13,27 @@ import {
 var REQUEST_URL = 'http://api.geonames.org/earthquakesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&username=bettyjing';
 
 class DisplayData extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
-  }
 
-  componentDidMount() {
-    this.fetchData();
-  }
+  state = { earthquakes: [] };
 
-  fetchData() {
+  componentWillMount() {
     fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.earthquakes),
-          loaded: true,
-        });
-      })
-      .done();
+    .then((response) => response.json())
+    .then((responseData) => this.setState({ articles: responseData.earthquakes }))
+    .catch(function(error) {
+    console.warn(error);
+    });
   }
 
   render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
-
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderEarthquakes}
-        style={styles.listView}
-      />
-    );
-  }
-
-  renderLoadingView() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading...
-        </Text>
-      </View>
-    );
-  }
-
-  renderEarthquakes(earthquakes) {
-    return (
-      <View style={styles.container}>
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{earthquakes.name}</Text>
-        </View>
+      <View>
+        {this.state.earthquakes.map(earthquake => (
+          <Text>{earthquakes.datetime}</Text>
+        ))}
       </View>
     );
   }
 }
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 12,
-    marginBottom: 3,
-    textAlign: 'left',
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
-  },
-});
 
 module.exports = DisplayData;
