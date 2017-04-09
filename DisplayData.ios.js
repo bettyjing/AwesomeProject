35536dd376c6a599a313991f72701e7c8acbba59
Samuel Wang <samuel.wang1995@gmail.com>
2017-04-08 19:14:47 -0700

    Loads listview for earthquakes and each property

--- DisplayData.ios.js
+++ DisplayData.ios.js
@@ -11,21 +11,21 @@ import {
 } from 'react-native';
 
 var REQUEST_URL = 'http://api.geonames.org/earthquakesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&username=bettyjing';
-
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 class DisplayData extends Component {
-
 
   constructor(props){
     super(props)
+
     this.state = {
-      earthquakes: []
+      earthquakes: ds.cloneWithRows([])
     };
   }
 
   componentDidMount() {
     fetch(REQUEST_URL)
     .then((response) => response.json())
-    .then((responseData) => this.setState({ earthquakes: responseData.earthquakes }))
+    .then((responseData) => this.setState({ earthquakes: ds.cloneWithRows(responseData.earthquakes) }))
     .catch(function(error) {
       console.warn(error);
     });
@@ -34,9 +34,18 @@ class DisplayData extends Component {
   render() {
     return (
       <View>
-        {this.state.earthquakes.map(earthquake => (
-          <Text>{earthquake.datetime}</Text>
-        ))}
+        <ListView
+          dataSource = {this.state.earthquakes}
+          renderRow={(rowData) => (
+            <View>
+              <Text>{rowData.src}</Text>
+              <Text>{rowData.datetime}</Text>
+              <Text>{rowData.magnitude}</Text>
+              <Text>{rowData.eqid}</Text>
+              <Text>{rowData.depth}</Text>
+            </View>
+          )}
+        />
       </View>
     );
   }
